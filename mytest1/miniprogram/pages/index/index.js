443cbcf4da31473517339f9c3421fcae22f3455f
Claranc <fxj_bupt@163.com>
2019-09-10 00:37:52 +0800

    fix

--- mytest1/miniprogram/pages/index/index.js
+++ mytest1/miniprogram/pages/index/index.js
@@ -8,7 +8,10 @@ Page({
     inputShowed: false, // 是否显示搜索框
     inputVal: "",
     category_box_width: 100, //分类总宽度
-    categories: ["上装", "裤装"]
+    categories: ['/images/1.jpg', 'images/add-addr.png', 'images/3.jpg'],
+    iconType: [
+      'success', 'success_no_circle', 'info', 'warn', 'waiting', 'cancel', 'download', 'search', 'clear'
+    ]
   },
 
   showInput: function () {
@@ -23,4 +26,35 @@ Page({
       inputShowed: false
     });
   },
+
+  goRowText: function () {
+    wx.navigateTo({
+      url: "/pages/rowText/rowText"
+    })
+  },
+
+  goColumnText: function () {
+    wx.navigateTo({
+      url: "/pages/columnText/columnText"
+    })
+  },
+
+  goIDCard: function () {
+    wx.navigateTo({
+      url: "/pages/idCard/idCard"
+    })
+  },
+
+  goBankCard: function () {
+    wx.navigateTo({
+      url: "/pages/bankCard/bankCard"
+    })
+  },
+
+  goLanguage: function () {
+    wx.navigateTo({
+      url: "/pages/language/language"
+    })
+  },
+
 })
